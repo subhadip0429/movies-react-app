abd878141c1d325adfd9ac1ff92364578d82fac1
subhadipfx <subhadip.dasgupta@datoms.io>
2021-10-31 01:02:17 +0530

    Updates

---
+++ precache-manifest.ea2d2bc004c25a7deebf5dc24452e4ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d1266d6d6228109d1588ed3df771fe",
+    "url": "/movies-react-app/index.html"
+  },
+  {
+    "revision": "82eb6417d8cbc99194a4",
+    "url": "/movies-react-app/static/css/main.f7a0a69e.chunk.css"
+  },
+  {
+    "revision": "65f163de5b6b78095c02",
+    "url": "/movies-react-app/static/js/2.0909dade.chunk.js"
+  },
+  {
+    "revision": "82eb6417d8cbc99194a4",
+    "url": "/movies-react-app/static/js/main.e4e2f543.chunk.js"
+  },
+  {
+    "revision": "8206c9efc9daa8329d9c",
+    "url": "/movies-react-app/static/js/runtime-main.4a2ee70f.js"
+  }
+]);
